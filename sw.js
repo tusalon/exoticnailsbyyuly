5a4ca3bb1375f6c4bea5b1062a018d3420e62eb3
TuDespensa25 <roma220130@gmail.com>
2026-05-08 17:45:51 -0400

    k

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker para Exotic Nails by Yuly
 
-const CACHE_NAME = 'exoticnailsbyyuly-v2';
+const CACHE_NAME = 'exoticnailsbyyuly-v3';
 const urlsToCache = [
   '/exoticnailsbyyuly/',
   '/exoticnailsbyyuly/index.html',
