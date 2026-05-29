c6be1d3b885c46be96b90afec16fb7cd8076ea7b
TuDespensa25 <roma220130@gmail.com>
2026-05-29 09:24:10 -0400

    Allow clients to reschedule bookings

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker para Exotic Nails by Yuly
 
-const CACHE_NAME = 'exoticnailsbyyuly-v35';
+const CACHE_NAME = 'exoticnailsbyyuly-v36';
 const urlsToCache = [
   '/exoticnailsbyyuly/',
   '/exoticnailsbyyuly/index.html',
