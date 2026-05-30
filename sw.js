9a346bcbea8cc0395e813432b506aff4f3def401
TuDespensa25 <roma220130@gmail.com>
2026-05-30 08:54:06 -0400

    Show waitlist on fully booked days

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker para Exotic Nails by Yuly
 
-const CACHE_NAME = 'exoticnailsbyyuly-v41';
+const CACHE_NAME = 'exoticnailsbyyuly-v42';
 const urlsToCache = [
   '/exoticnailsbyyuly/',
   '/exoticnailsbyyuly/index.html',
