c1b5d657708e40c390b9bbc1dc69f1991139ebfe
TuDespensa25 <roma220130@gmail.com>
2026-05-30 15:44:52 -0400

    Bajar minimo de duracion de servicios

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Service Worker para Exotic Nails by Yuly
 
-const CACHE_NAME = 'exoticnailsbyyuly-v45';
+const CACHE_NAME = 'exoticnailsbyyuly-v46';
 const urlsToCache = [
   '/exoticnailsbyyuly/',
   '/exoticnailsbyyuly/index.html',
